ffb6dc6144be4e3e8fc3cdde1c45406b1feb9c9d
Kishan Dedakia <kdedakia@gmail.com>
2013-09-21 10:26:03 -0400

    Twilio code

--- list.js
+++ list.js
@@ -8,26 +8,56 @@ var client = new twilio.RestClient('AC6ccc5ad275f60124a022af13ae9d4773', 'ca5dff
 var total_messages = 0;
 var new_messages = [];
 
-//Loop through a list of SMS messages sent from a given number
-client.listSms({
-    from:'+12247721893'
-}, function (err, responseData) {
-    
-    if (total_messages != responseData.smsMessages.length) {
-    	total_messages = responseData.smsMessages.length;
-    	console.log(total_messages);
-    	console.log(responseData.smsMessages[0].body);
-    }
 
-    
-    // responseData.smsMessages.forEach(function(message) {
-        // console.log('Message sent on: '+message.dateCreated.toLocaleDateString());
-        // console.log(message.body);
-    // });
+function check_sms() {
 
-});
+	//Loop through a list of SMS messages sent from a given number
+	client.listSms({
+	    from:'+12247721893'
+	}, function (err, responseData) {
+	    
+	    if (total_messages == 0) {
+	    	total_messages = responseData.smsMessages.length
+	    	console.log('set')
+	    }
+
+	    if (total_messages != responseData.smsMessages.length) {
+	    	total_messages = responseData.smsMessages.length;
+	    	console.log(total_messages);
+	    	console.log(responseData.smsMessages[0].body);
+	    }
+	    else {
+	    	console.log('unchanged')
+	    }
+
+	    console.log(responseData.smsMessages.length)
+
+	    
+	    // responseData.smsMessages.forEach(function(message) {
+	        // console.log('Message sent on: '+message.dateCreated.toLocaleDateString());n
+	        // console.log(message.body);
+	    // });
+
+	});
+
+}
+
+check_sms()
+setInterval ( check_sms, 1000 );
+
+
+
+
+
 
 /*
+	Single Phone
+	Lag Time: instantaneous
+
+
+
+
+
 	every X minutes check the SMS messages
 	if the # of messages has changed: 
 	check how much it has changed by
